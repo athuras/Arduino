34223f52cdf606b89b1ed1c893073aea0c44ebf4
Andy Lee <andylee.address@gmail.com>
2012-06-27 15:48:05 -0400

    added draft Message header file

---
+++ Message.h
@@ -0,0 +1,15 @@
+struct Message{
+	public:
+	//constructor
+	Message();
+	Message(byte col, byte cell, byte[] command);
+	
+	//serialize to string for Wire.write(char[]);
+	char[] serialize();
+	
+	//contents
+	byte col;
+	byte cell;
+	byte[8] command;
+
+}
