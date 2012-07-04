df8fb787cfed837ea15f08540f7a3fd2c9764a0d
Andy Lee <andylee.address@gmail.com>
2012-07-04 15:18:01 -0400

    Added slave test case impl

---
+++ test_case_slave/test_case_slave.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+
+char input[3];
+String output;
+
+void setup(){
+   Wire.begin(99); 
+   Wire.onReceive(receiveEvent);
+   Wire.onRequest(requestEvent);
+   output.reserve(24);
+}
+
+void loop(){
+  delay(100);
+}
+
+void receiveEvent(int howMany){
+    byte cnt = 0;
+    while (Wire.available() && cnt < 3){
+       char c = Wire.read();
+       input[cnt] = c;
+       cnt++;
+    }
+    if (input[0] == 'A'){
+       byte pinNum = input[1];
+       pinMode(pinNum, OUTPUT);
+       digitalWrite(pinNum, HIGH); 
+       output = "Pin is high";
+    }
+}
+
+void requestEvent(){
+   if (input[0] == 'A'){
+     Wire.write(output);
+   }  
+}
